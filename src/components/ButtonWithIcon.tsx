c5edb2e387fb0a9ddcd16a973ae479aa02439235
Glory Agatevure <agatevureglory@gmail.com>
2023-01-03 02:38:22 +0100

    change hover effect on button

--- src/components/ButtonWithIcon.tsx
+++ src/components/ButtonWithIcon.tsx
@@ -10,7 +10,7 @@ interface IButtonWithIcon {
 export default function ButtonWithIcon(params: IButtonWithIcon) : JSX.Element{
   return (
       <div className='w-full mx-4'>
-        <button className={`${params.myStyle} px-8 pt-2.5 pb-2 font-medium text-xs leading-normal rounded shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out flex align-center w-full`} onClick={params.action}>
+        <button className={`${params.myStyle} px-8 pt-2.5 pb-2 font-medium text-xs leading-normal rounded shadow-md hover:shadow-lg focus:bg-blue-200 focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out flex align-center w-full`} onClick={params.action}>
          <img src={params.image} alt="wallet-icon" width={36} />
           {params.text}
         </button>
